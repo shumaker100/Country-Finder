7fe4a53cea69fc8a58b4b820cf01cbdb6bcca7b3
shumaker100 <67166024+shumaker100@users.noreply.github.com>
2020-12-22 14:46:18 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import DemonynApp from "./demonymapp/demonymApp";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DemonynApp />
+      </div>
+    );
+  }
+}
+
+export default App;
